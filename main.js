de87302f86f2b74a37b484f04d32711c4dde51dc
Dara Adesoji <dara@unknown.home>
2025-11-12 14:53:35 +0000

    My first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+
+// use object factory to create humans with essence
+const createHuman = (name) => {
+    return {
+        name,
+        essence: {
+            strength: [],
+            power: [],
+            quickness: [],
+            dexterity: [],
+            endurance: [],
+            constitution: [],
+            coordination: [],
+            intellect: [],
+            reasoning: [],
+            perception: [],
+            insight: [],
+            resillience: [],
+            empathy: [],
+            fortitude: [],
+            aether_pool: [],
+            channeling: [],
+            aether_shaping: [],
+            aether_tolerance: [],
+            aether_resistance: [],
+        },
+        prime_aether: '',
+        minor_aether: '',
+        affinities: {
+            fire: 0, 
+            air: 0,
+            water: 0,
+            earth: 0,
+            lightning: 0,
+            spirit: 0,
+            nature: 0,
+            charm: 0,
+            illusion: 0,
+            clairvoyance: 0,
+            necromancy: 0,
+            celestial: 0,
+            abyssal: 0,
+            space: 0,
+            time: 0,
+            displacement: 0,
+            materialism: 0,
+            worlds: 0,
+            void: 0,
+            convergence: 0
+        }
+    }
+}
